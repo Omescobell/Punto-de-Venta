78f0f21c275683c11f0681e430383d2a68c2ae90
sfmdo <santiagomontesdeo@gmail.com>
2026-01-08 14:27:30 -0600

    Se cambio las choices para el role, y se corrigio la forma en la que se gestionaban los tiempos para el token

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -19,4 +19,6 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #path('api/users/<int:pk>/', ,name="users_detail"),
+    
 ]
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,23 +1,30 @@
 from django.db import models
 from django.urls import reverse
-from datetime import timedelta,timezone
+from django.utils import timezone 
+from datetime import timedelta
 
-class Users(models.Model):
-    name = models.CharField(max_length=150)
-    last_name= models.CharField(max_length=150)
-    password = models.CharField(max_length=128)
-    mobile_number = models.BigIntegerField()
-    email = models.CharField(max_length=254)
+class User(models.Model): 
+    username = models.CharField(max_length=150)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    password_hash = models.CharField(max_length=255) 
+    phone_number = models.CharField(max_length=20)
+    email = models.EmailField(max_length=254, unique=True) 
     address = models.CharField(max_length=200)
-    user_role = models.CharField(max_length=20)
+
+    ROLE_CHOICES = [
+        ('ADMIN', 'Admin'),
+        ('EMPLOYEE', 'Empleado'),
+        ('OWNER', 'Dueño'),
+    ]
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
     is_active = models.BooleanField(default=True)
-    
-    def get_absolute_url(self):
-        return reverse("users_detail", kwargs={"pk": self.pk})
     
     def __str__(self):
         return self.username
-    
+
+    def get_absolute_url(self):
+        return reverse("users_detail", kwargs={"pk": self.pk})
 
 def get_expiration_date():
     return timezone.now() + timedelta(days=1)
@@ -25,8 +32,11 @@ def get_expiration_date():
 class RefreshToken(models.Model): 
     token = models.TextField() 
     user_agent = models.CharField(max_length=255) 
-    ip_address = models.GenericIPAddressField() 
+    ip_address = models.GenericIPAddressField()
     expires_at = models.DateTimeField(default=get_expiration_date)
-    created_at = models.DateTimeField(default=timezone,auto_now=False,auto_now_add=False) 
+    created_at = models.DateTimeField(auto_now_add=True) 
     is_revoked = models.BooleanField(default=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="sessions")
+
+    def __str__(self):
+        return f"Token de {self.user.username}"
